6ee1506239e31e35847a1b0b5af3bfc965435b8f
Yuri Mikushov <mikushov.yu@gmail.com>
2021-01-07 16:54:36 +0300

    chore: renamed app copmonent

--- src/BugboardApp.jsx
+++ src/BugboardApp.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-function App() {
+function BugboardApp() {
   return <div>Soon</div>
 }
 
-export default App
+export default BugboardApp
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App.jsx'
+import Bugboard from './BugboardApp.jsx'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Bugboard />
   </React.StrictMode>,
   document.getElementById('root')
 )
