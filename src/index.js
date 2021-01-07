b1e94e423746bffd4670514355b32bc36ee1c355
Yuri Mikushov <mikushov.yu@gmail.com>
2021-01-07 17:24:16 +0300

    chore: added redux store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider as StoreProvider } from 'react-redux'
+import store from './redux/store'
 import './index.css'
 import Bugboard from './BugboardApp.jsx'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Bugboard />
+    <StoreProvider store={store}>
+      <Bugboard />
+    </StoreProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
