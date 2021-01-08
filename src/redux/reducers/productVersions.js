f2eef0699dc4a2396d8c7921e93714881a99ab09
Yuri Mikushov <mikushov.yu@gmail.com>
2021-01-08 18:56:30 +0300

    chore: corrected test data

--- src/redux/reducers/productVersions.js
+++ src/redux/reducers/productVersions.js
@@ -15,7 +15,6 @@ const productVersionList = [
     id: '2',
     versions: [
       { id: 'not_fixed', title: 'Not fixed' },
-      { id: '1_0_3', title: '1.0.3' },
       { id: '1_0_2', title: '1.0.2' },
       { id: '1_0_1', title: '1.0.1' },
       { id: '1_0_0', title: '1.0.0' },
@@ -26,10 +25,8 @@ const productVersionList = [
     id: '3',
     versions: [
       { id: 'not_fixed', title: 'Not fixed' },
-      { id: '1_0_2', title: '1.0.2' },
       { id: '1_0_1', title: '1.0.1' },
       { id: '1_0_0', title: '1.0.0' },
-      { id: '0_1_0', title: '0.1.0' },
     ],
   },
 ]
