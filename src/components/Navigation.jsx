caab4ae64fdc0d7bb14f5c7d67a2455fc1e0e3d0
Yuri Mikushov <mikushov.yu@gmail.com>
2021-01-08 19:01:22 +0300

    feature: added nav handling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,19 +1,64 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useRouteMatch } from 'react-router-dom'
+
+const styles = {
+  activeBreadcrumb: {
+    color: '#6c757d',
+    pointerEvents: 'none',
+    cursor: 'default',
+    textDecoration: 'none',
+  },
+}
+
+function useBreadcrumbs() {
+  const matchVersions = useRouteMatch('/products/:productId/versions')
+  const matchBugs = useRouteMatch('/products/:productId/versions/:versionId')
+
+  const breadcrumbs = [
+    {
+      title: 'Home',
+      href: '/',
+      isActive: !matchVersions && !matchBugs,
+    },
+  ]
+
+  if (matchVersions || matchBugs) {
+    breadcrumbs.push({
+      title: 'Product',
+      href: `/products/${matchVersions.params.productId}/versions`,
+      isActive: matchVersions.isExact,
+    })
+  }
+
+  if (matchBugs) {
+    breadcrumbs.push({
+      title: 'Bugs',
+      href: `/products/${matchBugs.params.productId}/versions/${matchBugs.params.versionId}`,
+      isActive: matchBugs.isExact,
+    })
+  }
+
+  return breadcrumbs
+}
 
 function Navigation() {
+  const breadcrumbs = useBreadcrumbs()
+
   return (
     <nav aria-label='breadcrumb'>
       <ol className='breadcrumb mt-2 mb-2'>
-        <li className='breadcrumb-item'>
-          <Link to='/'>Home</Link>
-        </li>
-        <li className='breadcrumb-item'>
-          <Link to='/versions'>Product name</Link>
-        </li>
-        <li className='breadcrumb-item active' aria-current='page'>
-          <Link to='/bugs'>Product version</Link>
-        </li>
+        {breadcrumbs.map((breadcrumb) => {
+          return (
+            <li className='breadcrumb-item' key={breadcrumb.title}>
+              <Link
+                to={breadcrumb.href}
+                style={breadcrumb.isActive ? styles.activeBreadcrumb : null}
+              >
+                {breadcrumb.title}
+              </Link>
+            </li>
+          )
+        })}
       </ol>
     </nav>
   )
