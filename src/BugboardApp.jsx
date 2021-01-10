844db3290c8e84d33545769a026e5e7a461980cd
Yuri Mikushov <mikushov.yu@gmail.com>
2021-01-10 18:24:44 +0300

    feature: deleted main page opening when app opening

--- src/BugboardApp.jsx
+++ src/BugboardApp.jsx
@@ -1,16 +1,8 @@
-import React, { useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
+import React from 'react'
 import Header from './components/Header'
 import Content from './components/Content'
 
 function BugboardApp() {
-  const history = useHistory()
-
-  useEffect(() => {
-    history.push('/')
-    // eslint-disable-next-line
-  }, [])
-
   return (
     <div style={{ maxWidth: '950px', margin: '0 auto' }}>
       <div className='container vh-100 shadow-lg bg-white'>
