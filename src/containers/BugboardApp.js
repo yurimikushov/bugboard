838c6850861e3835fce6311fcff72aecc80f5a3c
Yuri Mikushov <mikushov.yu@gmail.com>
2021-01-10 12:34:31 +0300

    chore: deleted unnecassery code

--- src/containers/BugboardApp.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux'
-import { loadProducts } from '../actions/products'
-import BugboardApp from '../BugboardApp'
-
-const mapDispatchToProps = (dispatch) => ({
-  loadProducts: () => dispatch(loadProducts()),
-})
-
-export default connect(null, mapDispatchToProps)(BugboardApp)
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider as StoreProvider } from 'react-redux'
 import store from './store'
 import './index.css'
-import Bugboard from './containers/BugboardApp'
+import Bugboard from './BugboardApp'
 
 ReactDOM.render(
   <React.StrictMode>
