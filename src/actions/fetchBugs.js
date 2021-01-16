6555853545f85cc5699ebf2bc6db1ca8b28a4261
Yuri Mikushov <mikushov.yu@gmail.com>
2021-01-16 10:20:49 +0300

    feature: added data fetching from fake api

--- src/actions/fetchBugs.js
+++ src/actions/fetchBugs.js
@@ -1,56 +1,23 @@
 import { FETCH_BUGS } from '../constants/ActionTypes'
 
-const commonBugs = [
-  {
-    id: 'bug-0001',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, soluta. Nam in libero unde inventore provident nostrum consectetur cupiditate rerum.',
-  },
-  {
-    id: 'bug-0002',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, soluta. Nam in libero unde inventore provident nostrum consectetur cupiditate rerum.',
-  },
-  {
-    id: 'bug-0003',
-    description:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure, tempore? Omnis similique ipsum qui fugiat magnam distinctio porro nostrum tenetur voluptatem. Harum quos architecto expedita, numquam inventore non in sit distinctio at quo tempore molestias saepe similique atque, ex maxime!',
-  },
-  {
-    id: 'bug-0004',
-    description:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure, tempore? Omnis similique ipsum qui fugiat magnam distinctio porro nostrum tenetur voluptatem. Harum quos architecto expedita, numquam inventore non in sit distinctio at quo tempore molestias saepe similique atque, ex maxime!',
-  },
-  {
-    id: 'bug-0005',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, soluta. Nam in libero unde inventore provident nostrum consectetur cupiditate rerum.',
-  },
-]
-
-const productVersionBugs = [
-  { productId: '1', versionId: 'not_fixed', bugs: commonBugs },
-  { productId: '1', versionId: '1_0_4', bugs: commonBugs },
-  { productId: '1', versionId: '1_0_3', bugs: commonBugs },
-  { productId: '2', versionId: '1_0_2', bugs: commonBugs },
-  { productId: '2', versionId: '1_0_1', bugs: commonBugs },
-]
-
 export default function fetchBugs(productId, versionId) {
-  const productVersion = productVersionBugs.filter(
-    (bug) => bug.productId === productId && bug.versionId === versionId
-  )[0]
-
-  const bugs = productVersion ? productVersion.bugs : []
-
   return (dispatch) => {
     dispatch(fetchBugsFetching())
 
-    // TODO: add fetch
-    new Promise((resolve, reject) => setTimeout(() => resolve(bugs), 500))
+    fetch(
+      `http://localhost:3004/products/${productId}/versions/${versionId}/bugs`
+    )
+      .then((res) => {
+        if (!res.ok) {
+          return Promise.reject(res)
+        }
+
+        return res.json()
+      })
       .then((bugs) => dispatch(fetchBugsSuccess(bugs)))
-      .catch((error) => {
-        dispatch(fetchBugsError(error))
+      .catch((err) => {
+        console.log(err)
+        dispatch(fetchBugsError("Couldn't get data :("))
       })
   }
 }
--- src/actions/fetchProductVersions.js
+++ src/actions/fetchProductVersions.js
@@ -1,50 +1,19 @@
 import { FETCH_PRODUCT_VERSIONS } from '../constants/ActionTypes'
 
-const productVersions = [
-  {
-    productId: '1',
-    versions: [
-      { id: 'not_fixed', title: 'Not fixed' },
-      { id: '1_0_4', title: '1.0.4' },
-      { id: '1_0_3', title: '1.0.3' },
-      { id: '1_0_2', title: '1.0.2' },
-      { id: '1_0_1', title: '1.0.1' },
-      { id: '1_0_0', title: '1.0.0' },
-      { id: '0_1_0', title: '0.1.0' },
-    ],
-  },
-  {
-    productId: '2',
-    versions: [
-      { id: 'not_fixed', title: 'Not fixed' },
-      { id: '1_0_2', title: '1.0.2' },
-      { id: '1_0_1', title: '1.0.1' },
-      { id: '1_0_0', title: '1.0.0' },
-      { id: '0_1_0', title: '0.1.0' },
-    ],
-  },
-  {
-    productId: '3',
-    versions: [],
-  },
-]
-
 export default function fetchProductVersions(productId) {
-  const versions = productVersions.filter(
-    (product) => product.productId === productId
-  )[0].versions
-
-  return (dispatch) => {
+  return async (dispatch) => {
     dispatch(fetchProductVersionFetching())
 
-    // TODO: add fetch
-    new Promise((resolve) => setTimeout(() => resolve(versions), 500))
-      .then((versions) => {
-        dispatch(fetchProductVersionSuccess(versions))
+    fetch(`http://localhost:3004/products/${productId}/versions`)
+      .then((res) => {
+        if (!res.ok) {
+          return Promise.reject(res)
+        }
+
+        return res.json()
       })
-      .catch((error) => {
-        dispatch(fetchProductVersionError(error))
-      })
+      .then((versions) => dispatch(fetchProductVersionSuccess(versions)))
+      .catch(() => dispatch(fetchProductVersionError("Couldn't get data :(")))
   }
 }
 
