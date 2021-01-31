09ada4949940980b07c0f1fec5ff11a5dda57e01
Yuri Mikushov <mikushov.yu@gmail.com>
2021-01-31 10:54:53 +0300

    feeture: added auto redirect on app running

--- src/containers/Content.jsx
+++ src/containers/Content.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Switch, Route, Redirect, useHistory } from 'react-router-dom'
 import withNavigation from './WithNavigation'
 import ProductList from './ProductList'
 import ProductVersionList from './ProductVersionList'
@@ -9,21 +9,28 @@ const ProductListWithNavigation = withNavigation(ProductList)
 const ProductVersionListWithNavigation = withNavigation(ProductVersionList)
 const BugListWithNavigation = withNavigation(BugList)
 
-const ContentContainer = () => (
-  <Switch>
-    <Route exact path='/'>
-      <ProductListWithNavigation />
-    </Route>
-    <Route exact path='/products/:productId/versions'>
-      <ProductVersionListWithNavigation />
-    </Route>
-    <Route exact path='/products/:productId/versions/:versionId/bugs'>
-      <BugListWithNavigation />
-    </Route>
-    <Route path='*'>
-      <Redirect to='/' />
-    </Route>
-  </Switch>
-)
+const ContentContainer = () => {
+  const history = useHistory()
+
+  // eslint-disable-next-line
+  useEffect(() => history.push('/'), [])
+
+  return (
+    <Switch>
+      <Route exact path='/'>
+        <ProductListWithNavigation />
+      </Route>
+      <Route exact path='/products/:productId/versions'>
+        <ProductVersionListWithNavigation />
+      </Route>
+      <Route exact path='/products/:productId/versions/:versionId/bugs'>
+        <BugListWithNavigation />
+      </Route>
+      <Route path='*'>
+        <Redirect to='/' />
+      </Route>
+    </Switch>
+  )
+}
 
 export default ContentContainer
