cf5ce9bb2512d72f1c047087d5a6991682f9adf5
Yuri Mikushov <mikushov.yu@gmail.com>
2021-03-23 08:59:43 +0300

    refactor: moved router

--- src/BugboardApp.jsx
+++ src/BugboardApp.jsx
@@ -1,16 +1,40 @@
 import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { Switch, Route, Redirect, useHistory } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Header from './containers/Header'
-import Content from './containers/Content'
+import ProductList from './containers/ProductList'
+import ProductVersionList from './containers/ProductVersionList'
+import BugList from './containers/BugList'
 
 const BugboardApp = ({ appTitle }) => {
+  const history = useHistory()
+
+  // eslint-disable-next-line
+  useEffect(() => history.push('/'), [])
+
   useEffect(() => (document.title = appTitle), [appTitle])
 
   return (
     <div className='container'>
       <Header />
-      <Content />
+      <main className='content container'>
+        <h2 className='content__title'>{appTitle}</h2>
+        <Switch>
+          <Route exact path='/'>
+            <ProductList />
+          </Route>
+          <Route exact path='/products/:productId/versions'>
+            <ProductVersionList />
+          </Route>
+          <Route exact path='/products/:productId/versions/:versionId/bugs'>
+            <BugList />
+          </Route>
+          <Route path='*'>
+            <Redirect to='/' />
+          </Route>
+        </Switch>
+      </main>
     </div>
   )
 }
